e2b17da1dec80c6e73cf49913388209346c0c915
jayro.alhuay <jayro.alhuay@delfosti.com>
2024-02-26 16:52:20 -0500

    Update language and title in index.html, add new dependencies in package-lock.json, import useSelector in App.tsx, and modify tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux"
 import { Home } from "./pages/Home"
+import { RootState } from "./store/store"
 function App() {
+  const { value } = useSelector((state: RootState) => state.theme)
   return (
-    <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-gray-50 dark:bg-gray-950
+    <div className={value}>
+      <div className="absolute top-0 bottom-0 z-[-2] min-h-screen w-full bg-gray-50 dark:bg-gray-950
     bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,216,255,0.5),rgba(255,255,255,0.9))]
     dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
 
-    <main className="container mx-auto">
-      <Home />
-    </main>
+        <main className="container mx-auto">
+          <Home />
+        </main>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,tsx}"],
-  extend: { darkMode: ['selector', '[data-mode="dark"]'],},
+  darkMode: ['selector'],
+  extend: { 
+   
+  },
   theme: {
   },
   plugins: [],
